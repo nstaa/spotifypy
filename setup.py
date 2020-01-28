d1764abbd2178fa56ed0d3e4b9ade628e9c8f683
Walrus <alexj0518@gmail.com>
2020-01-28 13:32:26 -0500

    ok

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='spotify.py',
+    version='0.0.1',
+    author='MrWalrus',
+    description=('No', 'description'),
+    license='MIT',
+    packages=['spotify', 'aiohttp']
+)
